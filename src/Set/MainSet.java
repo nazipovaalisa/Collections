1aaa88a3da78060a896a8480b65a605202cc07ad
nazipovaalisa <nazipovaa@gmail.com>
2022-10-24 22:45:08 +0300

    add treeset, hashset, linkedset

--- src/Set/MainSet.java
+++ src/Set/MainSet.java
@@ -1,11 +1,50 @@
 package Set;
 
-import java.util.HashSet;
-import java.util.TreeSet;
+import java.util.*;
 
 public class MainSet {
     public static void main(String[] args){
         TreeSet<Integer> tset = new TreeSet<>();
-        HashSet<Integer> hset = new HashSet<>()
+        HashSet<Integer> hset = new HashSet<>();
+        LinkedHashSet<Integer> lset = new LinkedHashSet<>();
+        Random r = new Random();
+        int min = 5;
+        int max = 1000;
+        for(int i=0; i<50; i++){
+            int t = r.nextInt(max - min) + min;
+            hset.add(t);
+            t = r.nextInt(max - min) + min;
+            lset.add(t);
+            tset.add(i+5);
+        }
+//        System.out.println(tset.size());
+//        System.out.println(hset.size());
+//        System.out.println(lset.size());
+        System.out.println("TreeSet");
+        MainSet.print(tset);
+        System.out.println("HashSet");
+        MainSet.print(hset);
+        System.out.println("LinkedHashSet");
+        MainSet.print(lset);
+        HashSet<Integer> hashSet1 =new HashSet<>(tset.subSet(10,16));
+        int count = 0;
+        for(int i: hashSet1){
+            if(tset.contains(i))
+                count++;
+        }
+        if(count==hashSet1.size())
+            System.out.println("TreeSet contains Hashset1");
+        else
+            System.out.println("TreeSet does not contain hashset1");
+        for(int i: hashSet1){
+            if(tset.contains(i))
+                tset.remove(i);
+        }
+        MainSet.print(tset);
+    }
+    static void print(Collection<Integer> c){
+        for( int i: c)
+            System.out.printf("%d ",i);
+        System.out.println();
     }
 }
